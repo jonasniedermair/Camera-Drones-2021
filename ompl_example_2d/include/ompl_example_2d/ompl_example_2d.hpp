1a5446593a8051aa1b438b03d200fce69e078aff
Elvir Crncevic <elvircrn@gmail.com>
2022-01-15 16:18:41 +0100

    Removed ompl

--- ompl_example_2d/include/ompl_example_2d/ompl_example_2d.hpp
+++
@@ -1,9 +0,0 @@
-/*
- * ompl_example_2d.hpp
- *
- *  Created on: April 6, 2020
- *      Author: Dominik Belter
- *	 Institute: Instute of Robotics and Machine Intelligence, Poznan University of Technology
- */
-
-#pragma once
